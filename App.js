40867fa4c37e495b44a207ab313e0dca33b24f28
Logan Truong <ttluan411@gmail.com>
2017-10-24 15:22:31 -0700

    yarn

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity, StatusBar} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import ViewA from './Components/ViewA';
 import ViewB from './Components/ViewB';
 
+StatusBar.setHidden(true);
 
 
 const SimpleApp = StackNavigator({
@@ -19,8 +20,8 @@ export default class App extends React.Component {
       so2:"",
       text: 'placeholder',
       result: '...',
-      dataSource: [{key: 'Devin'},
-      {key: 'Jackson'}]
+      // dataSource: [{key: 'Devin'},
+      // {key: 'Jackson'}]
     }
   }
   clickPlus(){
@@ -46,28 +47,8 @@ export default class App extends React.Component {
   render() {
     return(
     <View style={styles.wrapper}>
-      <TextInput
-      style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      onChangeText={(so1) => {
-        console.log(so1)
-        this.setState({so1})
-      }}
-      value={this.state.so1}
-      />
-      <TextInput
-      style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      onChangeText={(so2) => this.setState({so2})}
-      value={this.state.so2}
-      />
-      {/* <FlatList
-        data={this.state.dataSource}
-        renderItem={({item}) => <Text style={{padding:20,borderWidth:1}}>{item.key}</Text>}
-      /> */}
-      <TouchableOpacity style={styles.plus} onPress={this.clickPlus()}>
-        <Text> Cong </Text>
-      </TouchableOpacity>
-      <Text>{this.state.result}</Text>
     </View>
+
   );
   }
 }
@@ -81,3 +62,12 @@ const styles = StyleSheet.create({
     backgroundColor:'green'
   }
 });
+
+
+
+
+
+  /* <FlatList
+        data={this.state.dataSource}
+        renderItem={({item}) => <Text style={{padding:20,borderWidth:1}}>{item.key}</Text>}
+      /> */
