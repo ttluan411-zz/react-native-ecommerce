67a840a519a650043b426cc0374ab653a8b1d13b
Logan Truong <ttluan411@gmail.com>
2017-10-24 13:19:48 -0700

    php webservice talking

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import ViewA from './Components/ViewA';
 import ViewB from './Components/ViewB';
@@ -15,24 +15,59 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
+      so1:"",
+      so2:"",
       text: 'placeholder',
+      result: '...',
       dataSource: [{key: 'Devin'},
       {key: 'Jackson'}]
     }
+  }
+  clickPlus(){
+    fetch("http://localhost:8080/WebService01/Sum.php", {
+      method:"POST",
+      headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        "numberOne":this.state.so1,
+        "numberTwo":this.state.so2
+      })
+    })
+    .then((response)=>response.json())
+    .then((responseJson)=>{
+      this.setState({
+        result:responseJson.result
+      })
+    })
+    .catch((error)=>{console.log(error)})
   }
   render() {
     return(
     <View style={styles.wrapper}>
       <TextInput
       style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      onChangeText={(text) => this.setState({text})}
-      value={this.state.text}
-    />
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text style={{padding:20,borderWidth:1}}>{item.key}</Text>}
-        />
-      </View>
+      onChangeText={(so1) => {
+        console.log(so1)
+        this.setState({so1})
+      }}
+      value={this.state.so1}
+      />
+      <TextInput
+      style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={(so2) => this.setState({so2})}
+      value={this.state.so2}
+      />
+      {/* <FlatList
+        data={this.state.dataSource}
+        renderItem={({item}) => <Text style={{padding:20,borderWidth:1}}>{item.key}</Text>}
+      /> */}
+      <TouchableOpacity style={styles.plus} onPress={this.clickPlus()}>
+        <Text> Cong </Text>
+      </TouchableOpacity>
+      <Text>{this.state.result}</Text>
+    </View>
   );
   }
 }
@@ -41,5 +76,8 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: 'yellow'
+  },
+  plus: {
+    backgroundColor:'green'
   }
 });
